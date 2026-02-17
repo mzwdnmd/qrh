56274eb83e63c4f476a97dd0ee5c094adf2d6404
mzwdnmd <61614439+mzwdnmd@users.noreply.github.com>
2026-02-17 17:40:46 +0800

    config: github pages base

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+const REPO_NAME = "qrh"; // <- 如果你的仓库名不是 qrh，就改成实际仓库名
+
 export default defineConfig({
   plugins: [react()],
-})
+  base: `/${REPO_NAME}/`,
+});
